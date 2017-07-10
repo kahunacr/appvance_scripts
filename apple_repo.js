e8d3d8ab25aed6b561eca8257cbb5d532f65c02b
Administrator <Administrator@172.31.1.88>
2017-07-10 09:51:46 +0000

    Appvance Autocommit

---
+++ apple_repo.js
@@ -0,0 +1,8 @@
+_navigateTo("https://www.apple.com/");
+_click(_link("Mac"));
+_navigateTo("https://www.apple.com/ipad/");
+_click(_link("iPhone"));
+_click(_link("Watch"));
+_navigateTo("https://www.apple.com/tv/");
+_click(_link("Music"));
+_click(_span("iTunes"));
