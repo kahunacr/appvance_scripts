8cea3eadf59c17e050110332e37f45f9d9acf7ff
Abhishek <apandey@appvance.com>
2018-04-20 11:45:53 +0530

    Appvance commit - add wa.js

--- wa.js
+++ wa.js
@@ -1,5 +1,4 @@
 navigateTo("https://www.apple.com/");
 click(link("ac-gn-link ac-gn-link-mac"));
-wait("10000,isVisible(link("ac-gn-link ac-gn-link-ipad"))");
-//log(x);
+wait(10000,isVisible(link("ac-gn-link ac-gn-link-ipad")));
 click(link("ac-gn-link ac-gn-link-ipad"));
