1bce320eaa49d09532780610e5d1a26501bfa2ca
root <root@runner-snx6GCAX-project-6060102-concurrent-0>
2019-04-25 00:40:26 +0000

    Appvance commit - remove test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-navigateTo("https://www.facebook.com/");
-setValue(textbox("firstname"),"ijknlm");
-setValue(textbox("lastname"),"hbjn");
-setValue(byXPath("//*@data-fullfieldid='BO:93ffc8980cfe1c0a497a094662aa6f27d1821d02bb,FI:93ffc70c6a179e7135177f4034812c1acaf01b23d8'/input"),);
-setSelected(select("year"),"1913");
-setSelected(select("day"),"25");
-click(radio("sex[1]"));
